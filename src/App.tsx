3f0a65acaa46d42b8db39ff338817321bd7b27a4
aboudard <coco.boudard@gmail.com>
2025-02-05 16:47:52 +0100

    fix: routes lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { observer } from "mobx-react";
-import { useState } from "react";
+import { lazy, Suspense, useState } from "react";
 import { Route, Routes } from "react-router";
 import "./App.css";
-import Menu from "./components/menu/menu";
-import Home from "./features/home/home";
-import Todos from "./features/todos/todos";
 import AppStore from "./store/app-store";
 
 const App = observer(() => {
   const [store] = useState(() => new AppStore("Vite + React 18"));
+  const Home = lazy(() => import("./features/home/home"));
+  const Todos = lazy(() => import("./features/todos/todos"));
+  const Menu = lazy(() => import("./components/menu/menu"));
   return (
     <>
-      <Menu />
+      <Suspense>
+        <Menu />
+      </Suspense>
       <h1>{store.title}</h1>
       <div>
         <button onClick={() => store.setTitle("Modif Title")}>
@@ -19,10 +21,12 @@ const App = observer(() => {
         </button>
       </div>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/todos" element={<Todos />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/todos" element={<Todos />} />
+        </Routes>
+      </Suspense>
     </>
   );
 });
